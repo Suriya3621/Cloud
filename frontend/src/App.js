34ff32dbf470f8b726f7e6ae53a67e7b618d29bf
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-07-08 23:32:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,39 @@
-
-
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import NavBar from './Components/Layout/Navbar'; // Adjust the path if necessary
+import Login from './Components/Login';
+import Home from './Components/Home';
+import AdminPanel from './App/AdminPanel'
 function App() {
-  return (
-    <div className="App">
+  const [passCookies] = useCookies(['pass']);
 
-    </div>   
+  return (
+    <Router>
+      <div>
+      <AdminPanel/>
+      {
+//         <div style={{ position:"fixed",width:"100%"}}>
+//         <NavBar />
+//       </div>
+//         <div style={{ height: 3, width: "100%", backgroundColor: "blue" }}></div>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={passCookies.pass ? <Navigate to="/home" /> : <Navigate to="/login" />}
+//           />
+//           <Route
+//             path="/home"
+//             element={passCookies.pass ? <Home /> : <Navigate to="/login" />}
+//           />
+//           <Route
+//             path="/login"
+//             element={passCookies.pass ? <Navigate to="/home" /> : <Login />}
+//           />
+//         </Routes>
+      }
+      </div>
+    </Router>
   );
 }
 
