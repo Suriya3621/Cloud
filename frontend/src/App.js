286181cf6db18833bd8de23f7ee6bfeea0e082a9
Suriya3621 <158826855+Suriya3621@users.noreply.github.com>
2024-07-08 18:31:18 +0000

    ad all files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useCookies } from 'react-cookie';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import NavBar from './Components/Layout/Navbar'; // Adjust the path if necessary
-import Login from './Components/Login';
-import Home from './Components/Home';
-import AdminPanel from './App/AdminPanel'
+import Login from './Components/Login.jsx';
+import Home from './Components/Home.jsx';
+import AdminPanel from './App/AdminPanel.jsx'
+import NavBar from './Components/Layout/Navbar.jsx';
+
 function App() {
   const [passCookies] = useCookies(['pass']);
 
@@ -12,26 +13,24 @@ function App() {
     <Router>
       <div>
       <AdminPanel/>
-      {
-//         <div style={{ position:"fixed",width:"100%"}}>
-//         <NavBar />
-//       </div>
-//         <div style={{ height: 3, width: "100%", backgroundColor: "blue" }}></div>
-//         <Routes>
-//           <Route
-//             path="/"
-//             element={passCookies.pass ? <Navigate to="/home" /> : <Navigate to="/login" />}
-//           />
-//           <Route
-//             path="/home"
-//             element={passCookies.pass ? <Home /> : <Navigate to="/login" />}
-//           />
-//           <Route
-//             path="/login"
-//             element={passCookies.pass ? <Navigate to="/home" /> : <Login />}
-//           />
-//         </Routes>
-      }
+      
+      <div style={{ position:"fixed",width:"100%"}}>
+        <NavBar />
+       </div>
+       <div style={{ height: 3, width: "100%", backgroundColor: "blue" }}></div>
+        <Routes>
+         <Route
+             path="/"
+            element={passCookies.pass ? <Navigate to="/home" /> : <Navigate to="/login" />}
+          />
+           <Route
+             path="/home"
+           element={passCookies.pass ? <Home /> : <Navigate to="/login" />}/>
+           <Route
+             path="/login"
+           element={passCookies.pass ? <Navigate to="/home" /> : <Login />}
+          />
+         </Routes>      
       </div>
     </Router>
   );
