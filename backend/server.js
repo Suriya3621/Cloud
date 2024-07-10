21d1b52e7f23012e972e06ceab4983996c6130c4
Suriya3621 <suriyaprakashraja@gmail.com>
2024-07-10 19:16:46 +0530

    add all files

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ app.delete('/users/:id', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
