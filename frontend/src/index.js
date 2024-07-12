f4f2a29085bca1d541106481f0eed7fc47e54316
Suriya3621 <suriyaprakashraja@gmail.com>
2024-07-12 23:05:24 +0530

    update all files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,15 +8,15 @@ import { CookiesProvider } from "react-cookie";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-       <CookiesProvider>
-          <HelmetProvider>
-                        <App />
-          </HelmetProvider>
-      </CookiesProvider>
+    <CookiesProvider>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </CookiesProvider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
